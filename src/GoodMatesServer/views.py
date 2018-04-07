f1be5b837b7d85847308475deb802bd20c788cfa
JeremyMBell <bellfamilynumberone@gmail.com>
2018-04-07 18:25:17 -0400

    Indentation fix

--- src/GoodMatesServer/views.py
+++ src/GoodMatesServer/views.py
@@ -12,36 +12,36 @@ def jsonize(obj):
 		return jsonize([obj,])
 
 def parse_request(request):
-    if len(request.POST) == 1:
-        return json.loads(request.POST.keys()[0])
-    else:
-        to_return = []
-        for key in request.POST:
-            to_return.append(json.loads(key))
-        return to_return
+	if len(request.POST) == 1:
+		return json.loads(request.POST.keys()[0])
+	else:
+		to_return = []
+		for key in request.POST:
+			to_return.append(json.loads(key))
+		return to_return
 
 @csrf_exempt
 def create_user(request):
-    request = parse_request(request)
-    first_name = request.get("first_name")
-    last_name = request.get("last_name")
-    uid = request.get("uid")
+	request = parse_request(request)
+	first_name = request.get("first_name")
+	last_name = request.get("last_name")
+	uid = request.get("uid")
 
-    if first_name is not None and last_name is not None:
-    	user = User(first_name=first_name, last_name=last_name, uid=uid, registered=datetime.now())
-    	user.save()
-    	data = jsonize(user)
-    	return JsonResponse(json.loads(data), safe=False)
-    else:
-    	resp = HttpResponse("You did not enter a first name and/or last name")
-    	resp.status_code = 400
-    	return resp
+	if first_name is not None and last_name is not None:
+		user = User(first_name=first_name, last_name=last_name, uid=uid, registered=datetime.now())
+		user.save()
+		data = jsonize(user)
+		return JsonResponse(json.loads(data), safe=False)
+	else:
+		resp = HttpResponse("You did not enter a first name and/or last name")
+		resp.status_code = 400
+		return resp
 
 
 @csrf_exempt
 def create_group(request):
-    request = parse_request(request)
-    try:
+	request = parse_request(request)
+	try:
 		code = request.get("code")
 		userid = request.get("uid")
 		user = User.objects.get(uid=userid)
@@ -71,7 +71,7 @@ def create_group(request):
 
 @csrf_exempt
 def join_group(request):
-    request = parse_request(request)
+	request = parse_request(request)
 	try:
 		code = request.get("code")
 		userid = request.get("uid")
