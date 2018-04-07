2c80357831eab259709dcd934996e0e9f3d48ccd
JeremyMBell <bellfamilynumberone@gmail.com>
2018-04-07 14:11:02 -0400

    Models defined

--- README.md
+++ README.md
@@ -14,3 +14,29 @@
 Access in web browser at: localhost
 
 End server with Ctrl+C
+
+# File Structure in Django
+
+## `src/src`
+
+### settings.py
+
+File that just sets a bunch of options like where we're serving static files from or if we're in debug mode. Generally not going to use in project.
+
+### urls.py
+
+This defines urls using a regular expression and routes them to a 'View'.
+
+If you use a capturing group in the regex, you need it start with `?P<varname>` so it can pass invoke view(request, varname=captured_value). ex: `(?P<year>[0-9]{4})` will call view(request, year='2018') if you input 2018.
+
+In this project, we don't need to do anything fancy with urls.py, so each entry can be:
+
+`url(r'^url/endpoint/$', view)`
+
+Where view is a function from views.py
+
+### wsgi.py
+
+Never touched this file. You don't need to concern yourself with this.
+
+## `src/GoodMatesServer`
--- src/GoodMatesServer/models.py
+++ src/GoodMatesServer/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+class Group(models.Model):
+	id = models.AutoField(primary_key=True)
+
+class User(models.Model):
+	id = models.AutoField(primary_key=True)
+	first_name = models.CharField(max_length=20, blank=False)
+	last_name = models.CharField(max_length=20, blank=False)
+	registered = models.DateField(auto_now_add=True)
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+class Roommates(models.Model):
+	user1 = models.ForeignKey(User, on_delete=models.CASCADE)
+	user2 = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Laundry(models.Model):
+	start_time = models.DateField(blank=False)
+	end_time = models.DateField(blank=False)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+class Payment(models.Model):
+	creditor = models.OneToOneField(User, on_delete=models.CASCADE)
+	debtor = models.OneToOneField(User, on_delete=models.CASCADE)
+	amount = models.FloatField(blank = False)
+	time_due = models.DateField()
+	paid = models.BooleanField(blank = False)
+
+class Shower(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+	start_time = models.DateField(blank=False)
+	end_time = models.DateField(blank=False)
+
+class Guests(models.Model):
+	sock_on_door = models.BooleanField(blank = False)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+	start_time = models.DateField(blank=False)
+	end_time = models.DateField(blank=False)
+	num_guests = models.IntegerField(blank = False, validators=[MinValueValidator(1)])
+
+class Plan(models.Model):
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+	start_time = models.DateField(blank=False)
+	end_time = models.DateField(blank=False)
+	notes = models.CharField(max_length=500)
+
+class Chore(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+	notes = models.CharField(max_length=250)
+	due = models.DateField(blank=False)
+
+class PostIt(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	group = models.ForeignKey(Group, on_delete=models.CASCADE)
+	notes = models.CharField(max_length=250)
+	closed = models.BooleanField(blank = False)
