b5d4a2d0e0afe3423262e6bea5770bf16bba6cbf
JeremyMBell <bellfamilynumberone@gmail.com>
2018-04-07 15:08:31 -0400

    Create user view

--- src/GoodMatesServer/views.py
+++ src/GoodMatesServer/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.core import serializers
+from GoodMatesServer.models import User
+from django.http import JsonResponse, HttpResponse
+import json
 
-# Create your views here.
+def create_user(request):
+	first_name = request.POST.get("first_name")
+	last_name = request.POST.get("last_name")
+
+	if first_name is not None and last_name is not None:
+		user = User(first_name=first_name, last_name=last_name)
+		user.save()
+		data = serializers.serialize("json", user)
+		return JsonResponse(json.loads(data))
+
+	resp = HttpResponse("You did not enter a first name and/or last name")
+	resp.status_code = 400
+	return resp
+	
